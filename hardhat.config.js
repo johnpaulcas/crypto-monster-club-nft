9e2eca2d6cba3cd0302fadf95c5909e14be3d03b
John Paul Cas <casjp.app@gmail.com>
2021-10-11 04:47:00 +0800

    🔧 update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
+  paths: {
+    artifacts: './sample/src/artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/Yt7SwaEBlKVfnkBc9XFzIuwyBZUUrfON",
+      
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_ETHERSCAN_KEY_LIVE
+  }
 };
