b29d50231919587986c5c7c472146f884f08d8eb
John Paul Cas <casjp.app@gmail.com>
2021-10-16 17:42:47 +0800

    update cmc

--- contracts/CryptoMonsterClub.sol
+++ contracts/CryptoMonsterClub.sol
@@ -12,7 +12,7 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
     using SafeMath for uint;
     
     uint constant public MONSTER_TOTAL_SUPPLY = 7777;
-    uint constant public MONSTER_RESERVED = 200;
+    uint MONSTER_RESERVED = 200; 
     
     uint constant public PORTAL_PER_USER_ALLOWED = 1;
     uint constant public PRE_MINT_LIMIT_PER_TOKEN = 3;
@@ -20,18 +20,22 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
     
     uint private basePrice = 0.055 ether;
     
-    bool public isPreMintOpen = false;
+    bool public isPreMintOpen = true;
     bool public isPublicMintOpen = false;
     
     string _baseTokenURI;
-    address public portalAddress;
+    address public portalAddress = 0x27f46c04F842636add51a2506C02bf5A6954aD9D;
     
     mapping(uint => uint) public mintedPerTokenTracker;
     
     event Mint(address owner, uint tokenId);
     
-    constructor(address _addrs) ERC721("CryptoMonsterClub", "CMC") {
-        portalAddress = _addrs;
+    constructor() ERC721("CryptoMonsterClub", "CMC") {
+        MONSTER_RESERVED = MONSTER_RESERVED.sub(3);
+        for (uint monster = 0; monster < 3; monster ++) {
+            _safeMint(_msgSender(), totalSupply().add(1));
+            emit Mint(_msgSender(), totalSupply());
+        }
     }
     
     function premint() external payable {
@@ -42,7 +46,7 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
         require(balanceOfUser > 0, "CryptoMonsterClub: Only portal holder allowed.");
         require(balanceOfUser == PORTAL_PER_USER_ALLOWED, "CryptoMonsterClub: Allowed 1 portal per address only.");
         
-        uint remainder = msg.value.mod(basePrice); 
+        uint remainder = msg.value.mod(basePrice); // make sure that it divisible by 0.055
         require(remainder == 0, "CryptoMonsterClub: eth should be divisible by 0.055.");
         
         uint numberOfTokenToBeMinted = msg.value.div(basePrice);
@@ -50,6 +54,7 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
         uint numberOfMintedInToken = mintedPerTokenTracker[tokenId];
         require(numberOfMintedInToken <= PRE_MINT_LIMIT_PER_TOKEN, "CryptoMonsterClub: Only 3 monster can be minted per portal.");
         require(numberOfTokenToBeMinted > 0 && numberOfTokenToBeMinted <= PRE_MINT_LIMIT_PER_TOKEN, "CryptoMonsterClub: min 1 and max is 3 per portal");
+        require(totalSupply().add(numberOfTokenToBeMinted) <= MONSTER_TOTAL_SUPPLY.sub(MONSTER_RESERVED), "CryptoMonsterClub: Sold out.");
         require(msg.value >= numberOfTokenToBeMinted.mul(basePrice), "CryptoMonsterClub: Insuffient Eth");
         
         for (uint256 num = 0; num < numberOfTokenToBeMinted; num ++) {
@@ -60,14 +65,13 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
     
     function mint() external payable {
         require(isPublicMintOpen, "CryptoMonsterClub: Public mint not yet open.");
-        require(totalSupply() <= MONSTER_TOTAL_SUPPLY.sub(MONSTER_RESERVED), "Sold out");
         
         uint remainder = msg.value.mod(basePrice);
         uint numberOfTokenToBeMinted = msg.value.div(basePrice);
         uint allSupply = totalSupply().add(numberOfTokenToBeMinted);
         require(remainder == 0, "CryptoMonsterClub: eth should be divisible by 0.055.");
         require(numberOfTokenToBeMinted > 0 && numberOfTokenToBeMinted <= PUB_MINT_LIMIT_PER_TX, "CryptoMonsterClub: min 1 and max 10 per tx.");
-        require(allSupply <= MONSTER_TOTAL_SUPPLY.sub(MONSTER_RESERVED), "CryptoMonsterClub: only few token remaining.");
+        require(allSupply <= MONSTER_TOTAL_SUPPLY.sub(MONSTER_RESERVED), "CryptoMonsterClub: Sold out.");
         for (uint i = 0; i < numberOfTokenToBeMinted; i++) {
             _safeMint(_msgSender(), totalSupply().add(1));
             emit Mint(_msgSender(), totalSupply());
@@ -96,11 +100,24 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
         return _baseTokenURI;
     }
     
+    function getReserveSupply() external view returns(uint) {
+        return MONSTER_RESERVED;
+    }
+
     function airdrop(address[] memory _airdropAddrs) external onlyOwner() {
         require(totalSupply().add(_airdropAddrs.length) <= MONSTER_TOTAL_SUPPLY, "CryptoMonsterClub: exceed supply.");
+        require(MONSTER_RESERVED.sub(_airdropAddrs.length) > 0);
+        MONSTER_RESERVED = MONSTER_RESERVED.sub(_airdropAddrs.length);
         for (uint i = 0; i < _airdropAddrs.length; i++) {
             _safeMint(_airdropAddrs[i], totalSupply().add(1));
             emit Mint(_airdropAddrs[i], totalSupply());
+        }
+    }
+    
+    function mintAndTransfer(address _to, uint numberOfToken) external onlyOwner() {
+        require(totalSupply().add(numberOfToken) <= MONSTER_TOTAL_SUPPLY, "CryptoMonsterClub: mint exceed supply.");
+        for (uint i = 0; i < numberOfToken; i++) {
+            _safeMint(_to, totalSupply().add(1));
         }
     }
     
@@ -122,6 +139,10 @@ contract CryptoMonsterClub is ERC721Enumerable, ERC721Pausable, Ownable {
     
     function updateBaseUri(string memory _newUri) external onlyOwner() {
         _baseTokenURI = _newUri;
+    }
+    
+    function updatePortal(address _portal) external onlyOwner() {
+        portalAddress = _portal;
     }
     
     function withdrawAll() external onlyOwner() {
